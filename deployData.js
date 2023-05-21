1f12d603407ead2e16a8c04d86c88844560c9827
Rastra Ay <rastraay20@gmail.com>
2023-05-22 04:38:21 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,7 @@
+const studentName = 'Rastra Dequelhart Adichandra Ay';
+
+const studentId = 'FE5604945';
+
+const NetlifyDeployUrl = 'TODO: replace me';
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
