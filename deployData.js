725dcc56754e5105211c05c5095f66967deba958
Rastra Ay <rastraay20@gmail.com>
2023-05-22 13:42:06 +0700

    second commit

--- deployData.js
+++ deployData.js
@@ -2,6 +2,6 @@ const studentName = 'Rastra Dequelhart Adichandra Ay';
 
 const studentId = 'FE5604945';
 
-const NetlifyDeployUrl = 'TODO: replace me';
+const NetlifyDeployUrl = 'https://rastraay-fe5604945.netlify.app/client/index.html';
 
 module.exports = { studentName, studentId, NetlifyDeployUrl };
